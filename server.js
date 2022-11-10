3d16ae073ce0f8faaeddf891446e95b3bf63715e
Jamal Rizki <jamalbrizki@gmail.com>
2022-11-09 19:15:24 -0500

    pt 2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const PORT = process.env.PORT
 
 app.get('/', (req, res) => {
-  res.send('Hello, world!')
+  res.send('Hello, world pt 2!')
 })
 
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`))
